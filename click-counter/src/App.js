78facd84e09675bb1763814a81b46638e2cc496c
Phuc Le <phuclevinh2000@gmail.com>
2022-01-06 00:30:29 +0200

    Started testing

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -5,14 +5,8 @@ function App() {
   const [count, setCount] = React.useState(0);
 
   return (
-    <div data-test='component-app' className='App'>
-      <h1 data-test='counter-display'>
-        The counter is currently&nbsp;
-        <span data-test='count'>{count}</span>
-      </h1>
-      <button data-test='increment-button' onClick={() => setCount(count + 1)}>
-        Increment counter
-      </button>
+    <div data-test='component-app'>
+      <h1>App</h1>
     </div>
   );
 }
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -4,4 +4,16 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('render without error', () => {});
+test('render without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('render increment button', () => {});
+
+test('render counter display', () => {});
+
+test('counter display start at 0', () => {});
+
+test('clicking button increment counter display', () => {});
