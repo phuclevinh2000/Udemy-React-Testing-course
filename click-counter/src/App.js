d878a3350edb3e60302eec37a819e375ee18307c
Phuc Le <phuclevinh2000@gmail.com>
2022-01-06 00:38:01 +0200

    remove the data-test from production mode

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   const [count, setCount] = React.useState(0);
 
-  return (
-    <div data-test='component-app'>
-      <h1>App</h1>
-    </div>
-  );
+  return <div data-test='component-app'></div>;
 }
 
 export default App;
